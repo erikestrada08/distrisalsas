9f12baf9114d89a0bcaff1cd2e1695681c933ea7
erikestrada08 <estrada.erik321@gmail.com>
2025-10-31 21:10:54 -0500

    feat: agregar y eliminar atributos interfaz gestion producto

--- resources/js/Modules/GestionProductos.vue
+++ resources/js/Modules/GestionProductos.vue
@@ -249,7 +249,61 @@
                         </div>
                     </div>
                     <!-- tabla -->
-                    <div>
+
+                    <div class="flex flex-col gap-1">
+
+                        <!-- atributos del producto -->
+                        <div class="flex flex-col gap-1 rounded-md p-2 shadow-md overflow-x-auto bg-card text-foreground border border-border">
+
+                            <label for="atributo" class="text-sm">Atributos</label>
+
+                            <div class="flex gap-1">
+                                <div class="relative flex-1">
+                                    <input type="text" id="atributo" class="border border-border bg-background text-foreground rounded-md py-1 pl-8 pr-1 w-full" v-model.trim="atributo">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 absolute top-1/2 left-2 -translate-y-1/2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                    </svg>
+                                </div>
+                                <button 
+                                title="agregar atributo"
+                                class="bg-green-500 w-10 flex justify-center items-center"
+                                @click.prevent="agregarAtributo"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                                    </svg>
+                                </button>
+                            </div>
+
+                            <div 
+                            v-if="listaAtributos.length > 0"
+                            class="flex flex-col gap-1"
+                            >
+                                <div 
+                                v-for="(atributo, i) in listaAtributos"
+                                :key="i"
+                                class="flex items-center gap-1"
+                                >
+                                    <span class="w-1/4">{{ atributo }}:</span> 
+                                    <input 
+                                    type="text" 
+                                    class="border border-border bg-background text-foreground rounded-md p-1 w-3/4"
+                                    v-model.trim="producto.atributos[atributo]"
+                                    >
+                                    <button 
+                                    class="bg-red-400 h-full w-10 flex justify-center items-center"
+                                    @click.prevent="eliminarAtributo(atributo)"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+
                         <div class="rounded-md p-2 shadow-md overflow-x-auto bg-card text-foreground border border-border">
                             <DataTable 
                             :value="productosComputada" 
@@ -402,12 +456,8 @@
             </Transition>
         </div>
 
-
-    <!-- </PlantillaAdministrativa> -->
 </template>
 <script>
-// import { Head } from '@inertiajs/vue3';
-// import PlantillaAdministrativa from '@/Layouts/plantillaAdministrativa.vue';
 import VueEasyLightbox from 'vue-easy-lightbox';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
@@ -415,8 +465,6 @@ import { datatablePt } from '@/utils/themePrimevue';
 
 export default {
     components:{
-        // Head,
-        // PlantillaAdministrativa,
         VueEasyLightbox,
         DataTable,
         Column
@@ -433,7 +481,8 @@ export default {
                 imagen: null,
                 marca_id: null,
                 categoria_id: null,
-                activo: true
+                activo: true,
+                atributos: {}
             },
             marcas: [],
             productos: [],
@@ -454,7 +503,9 @@ export default {
             filtro: null,
             filters: {
                 global: { value: null, matchMode: 'contains' }
-            }
+            },
+            atributo: null,
+            listaAtributos: []
         }
     }, 
     computed: {
@@ -617,7 +668,18 @@ export default {
             Object.entries( this.$data[_formulario] ).forEach( ([key, value]) => {
                 this.$data[_formulario][key] = null;
             } );
+        },
+
+        agregarAtributo(){
+            this.listaAtributos.push(this.atributo);
+            this.atributo = null;
+        },
+
+        eliminarAtributo(_atributo){
+            this.listaAtributos = this.listaAtributos.filter(atributo => atributo != _atributo);
+            delete this.producto.atributos[_atributo];
         }
+
     },
     mounted() {
         this.obtenerMarcas(); 
